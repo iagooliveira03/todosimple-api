e9a0617177f5401252794e8fe406c7c1dbd9ba9d
iagooliveira03 <iagotcoliveira07@gmail.com>
2024-10-10 15:35:25 -0300

    Refactor UserService

--- src/main/java/com/lucasangelo/todosimple/repositories/UserRepository.java
+++ src/main/java/com/lucasangelo/todosimple/repositories/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lucasangelo.todosimple.models.User;
-import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
